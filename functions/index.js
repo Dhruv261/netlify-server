eb28771432acc45924a55d173c067e4fdd81f715
Dhruv <55016342+Dhruv261@users.noreply.github.com>
2023-05-11 11:31:07 +0530

    main

---
+++ functions/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const serverless = require('serverless-http');
+const app = express();
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.json({
+    path: 'Home',
+  });
+});
+
+router.get('/json', (req, res) => {
+  res.json({
+    path: '/json',
+  });
+});
+
+app.use('/', router);
+
+module.exports.handler = serverless(app);
